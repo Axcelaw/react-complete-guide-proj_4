c4834a4f3c248eabd76b3a9fa33fe1a5078947cc
Axcelaw <walace.andr.m@gmail.com>
2023-02-15 10:45:04 -0300

    Updated Login component's useEffect function with setTimeout().

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,14 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    console.log('Checking form validity!');
-    setTimeout(() => {
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
       setFormIsValid(
         enteredEmail.includes('@') && enteredPassword.trim().length > 6
       );
     }, 500);
 
-    return () => {};
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
